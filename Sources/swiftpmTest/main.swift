be76076729590a2d820dfa83af800cf4228c5971
daikimat <daiki-matsumoto@cybozu.co.jp>
2021-08-03 09:55:07 +0900

    ライブラリを使用

--- Sources/swiftpmTest/main.swift
+++ Sources/swiftpmTest/main.swift
@@ -1,1 +1,15 @@
+import ArgumentParser
+
+struct SwiftpmTest: ParsableCommand {
+  @Argument(help: "引数１")
+  var arg1: String
+
+  mutating func run() throws {
+    print(self.arg1)
+    print(self.arg1)
+    print(self.arg1)
+  }
+}
+
 print("Hello, world!")
+SwiftpmTest.main()
